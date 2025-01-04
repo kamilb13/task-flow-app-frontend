36d1141b66efb5b06d34e2a708306616dc5f7531
kamilb13 <kamil.brzoza13@gmail.com>
2025-01-04 20:41:00 +0100

    tasks adding functionality

--- src/api/tasks.tsx
+++ src/api/tasks.tsx
@@ -1,24 +1,26 @@
 import axiosInstance from "./axiosInstance.tsx";
 
-// export const createTask = async (newTask) => {
-//     const token = localStorage.getItem("token");
-//     const userId = localStorage.getItem("userid");
-//
-//     try {
-//         return await axiosInstance.post('/create-task', newTask);
-//     } catch (error) {
-//         console.error('Error creating board:', error);
-//     }
-// }
+export const createTask = async (newTask) => {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userid");
+    console.log(newTask);
+    try {
+        return await axiosInstance.post('/create-task', newTask, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+        });
+    } catch (error) {
+        console.error('Error creating board:', error);
+    }
+}
 
 export const fetchTasks = async (boardId) => {
     const token = localStorage.getItem("token");
     const userId = localStorage.getItem("userid");
 
     try {
-        const response = await axiosInstance.get(`/get-tasks?boardId=${boardId}`)
-        //setTasks(response.data);
-        return response;
+        return await axiosInstance.get(`/get-tasks?boardId=${boardId}`);
     } catch (e) {
         console.error("Błąd podczas pobierania boards:", e);
     }
--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from 'react';
-import {fetchTasks} from "../../api/tasks.tsx";
+import {createTask, fetchTasks} from "../../api/tasks.tsx";
 import {useLocation} from "react-router-dom";
-import {Button} from "react-bootstrap";
+import {Button, Form, FormControl, FormGroup, Modal, ModalTitle} from "react-bootstrap";
 
 const Tasks = () => {
     const [tasks, setTasks] = useState([]);
     const [loading, setLoading] = useState(true);
     const location = useLocation();
     const {boardId} = location.state || {};
+    const [showModalAddTask, setShowModalAddTask] = useState(false);
+    const [taskName, setTaskName] = useState('');
 
     useEffect(() => {
         fetchTasksData();
@@ -32,17 +34,40 @@ const Tasks = () => {
         }
     };
 
+    const toggleModalAddTask = () => {
+        setShowModalAddTask((prev) => !prev);
+    };
+
+    const handleFormSubmit = async () => {
+        //TODO !!!!
+        const newTask = {
+            title: "zadanie 1",
+            description: "opis zadania do wykonania",
+            userId: 2,
+            board: {
+                "id": 1
+            }
+        }
+        const response = await createTask(newTask);
+        if (response?.status === 201) {
+            console.log("Add task!" + taskName);
+            toggleModalAddTask();
+            fetchTasksData();
+        }
+
+    }
+
     if (loading) return <h2>Loading tasks...</h2>;
 
     return (
         <div>
             <h1>Tasks</h1>
-            <Button variant="primary" >Add task</Button>
+            <Button variant="primary" onClick={toggleModalAddTask}>Add task</Button>
             {tasks.length > 0 ? (
                 <ul>
                     {tasks.map((task) => (
                         <li key={task.id}>
-                            {task.title}, {task.description}
+                            {task.title}, {task.description}, {task.status}
                             <Button>
                                 Delete task
                             </Button>
@@ -55,6 +80,27 @@ const Tasks = () => {
             ) : (
                 <h3>No tasks found</h3>
             )}
+            <Modal show={showModalAddTask} onHide={toggleModalAddTask}>
+                <Modal.Header>
+                    <ModalTitle>Add task</ModalTitle>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <FormGroup>
+                            <FormControl
+                                onChange={(e) => setTaskName(e.target.value)}
+                                type="text"
+                                placeholder="Enter board name"
+                                required
+                            />
+                        </FormGroup>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={toggleModalAddTask}>Close</Button>
+                    <Button variant="secondary" onClick={handleFormSubmit}>Add task!</Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
