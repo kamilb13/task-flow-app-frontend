40b47e78e50e8d0ebf5727e7534bed8c96c0c06a
kamilb13 <kamil.brzoza13@gmail.com>
2025-01-06 00:17:47 +0100

    added editing and deleting functionality for tasks

--- src/api/tasks.tsx
+++ src/api/tasks.tsx
@@ -15,7 +15,7 @@ export const createTask = async (newTask) => {
     }
 }
 
-export const fetchTasks = async (boardId) => {
+export const fetchTasks = async (boardId: number) => {
     const token = localStorage.getItem("token");
     const userId = localStorage.getItem("userid");
 
@@ -23,5 +23,68 @@ export const fetchTasks = async (boardId) => {
         return await axiosInstance.get(`/get-tasks?boardId=${boardId}`);
     } catch (e) {
         console.error("Błąd podczas pobierania boards:", e);
+    }
+}
+
+export const changeTaskStatus = async (taskId, newStatus) => {
+    const token = localStorage.getItem("token");
+    console.log(newStatus.toUpperCase());
+    try {
+        return await axiosInstance.post(
+            `/change-status?taskId=${taskId}&newStatus=${newStatus.toUpperCase()}`,
+            {},
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+    } catch (e) {
+        console.error("Błąd podczas zmieniania statusu taska:", e);
+    }
+}
+
+export const deleteTask = async (taskId) => {
+    const token = localStorage.getItem("token");
+    console.log(taskId);
+    try {
+        return await axiosInstance.delete(`/delete-task?taskId=${taskId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    } catch (e) {
+        console.error("Błąd podczas usuwania zadania:", e);
+    }
+}
+
+export const editTasks = async (taskToEditId, taskName, taskDescription, boardId) => {
+    const token = localStorage.getItem("token");
+    const editTask = {
+        id: taskToEditId,
+        title: taskName,
+        description: taskDescription,
+        userId: localStorage.getItem("userid"),
+        board: {
+            id: boardId
+        }
+    }
+    console.log("----------------------");
+    console.log(editTask);
+    console.log(token);
+    console.log(taskToEditId);
+    console.log("----------------------");
+    try {
+        return await axiosInstance.put(`/edit-task`,
+            editTask,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        //return response;
+    } catch (error) {
+        console.error('Error deleting board:', error);
     }
 }
--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import {createTask, fetchTasks} from "../../api/tasks.tsx";
+import {changeTaskStatus, createTask, deleteTask, editTasks, fetchTasks} from "../../api/tasks.tsx";
 import {useLocation} from "react-router-dom";
-import {Button, Form, FormControl, FormGroup, Modal, ModalTitle} from "react-bootstrap";
+import {Button, Form, FormCheck, FormControl, FormGroup, Modal, ModalTitle} from "react-bootstrap";
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 
 const Tasks = () => {
     const [tasks, setTasks] = useState([]);
@@ -10,6 +11,20 @@ const Tasks = () => {
     const {boardId} = location.state || {};
     const [showModalAddTask, setShowModalAddTask] = useState(false);
     const [taskName, setTaskName] = useState('');
+    const [taskDescription, setTaskDescription] = useState('');
+    const [status, setStatus] = useState('');
+    const [taskIdToChangeStatus, setTaskIdToChangeStatus] = useState('');
+    const [taskEditModal, setTaskEditModal] = useState(false);
+    const [taskToEditId, setTaskToEditId] = useState('');
+
+    const [statusChangeModal, setStatusChangeModal] = useState(false);
+    const toggleModalChange = () => {
+        setStatusChangeModal(!statusChangeModal);
+    }
+
+    const toggleModalEditTask = () => {
+        setTaskEditModal(!taskEditModal);
+    }
 
     useEffect(() => {
         fetchTasksData();
@@ -39,25 +54,74 @@ const Tasks = () => {
     };
 
     const handleFormSubmit = async () => {
-        //TODO !!!!
+        if (taskName === '' || taskDescription === '') {
+            return;
+        }
         const newTask = {
-            title: "zadanie 1",
-            description: "opis zadania do wykonania",
-            userId: 2,
+            title: taskName,
+            description: taskDescription,
+            userId: localStorage.getItem("userid"),
             board: {
-                "id": 1
+                id: boardId
             }
         }
         const response = await createTask(newTask);
         if (response?.status === 201) {
-            console.log("Add task!" + taskName);
+            // console.log("Add task!" + taskName);
             toggleModalAddTask();
+            setTaskName('');
+            setTaskDescription('');
             fetchTasksData();
         }
+    }
+
+    const handleChangeStatus = () => {
 
     }
 
+
+
     if (loading) return <h2>Loading tasks...</h2>;
+
+    const submitCheckBox = async () => {
+        console.log("submitCheckBox", status);
+        try {
+            const response = await changeTaskStatus(taskIdToChangeStatus, status);
+            console.log("RESPONSE " + response);
+            if (response?.status === 200) {
+                fetchTasksData();
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+        toggleModalChange();
+        setStatus(null);
+    }
+
+    const handleStatusChange = (e) => {
+        const {value} = e.target;
+        setStatus(value);
+    };
+
+    const handelDeleteTask = async (taskId) => {
+        try {
+            const response = await deleteTask(taskId);
+            console.log(response?.status);
+            if (response?.status === 200) {
+                fetchTasksData();
+            }
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+
+    const handelEditTask = async () => {
+        try {
+            const response = await editTasks(taskToEditId, taskName, taskDescription, boardId);
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
 
     return (
         <div>
@@ -67,12 +131,23 @@ const Tasks = () => {
                 <ul>
                     {tasks.map((task) => (
                         <li key={task.id}>
-                            {task.title}, {task.description}, {task.status}
-                            <Button>
+                            {task.id}, {task.title}, {task.description}, {task.status}
+                            <Button onClick={() => handelDeleteTask(task.id)}>
                                 Delete task
                             </Button>
-                            <Button>
+                            <Button onClick={() => {
+                                toggleModalEditTask();
+                                setTaskName(task.title);
+                                setTaskDescription(task.description);
+                                setTaskToEditId(task.id);
+                            }}>
                                 Edit task
+                            </Button>
+                            <Button onClick={() => {
+                                toggleModalChange();
+                                setTaskIdToChangeStatus(task.id);
+                            }}>
+                                Change Status
                             </Button>
                         </li>
                     ))}
@@ -80,6 +155,64 @@ const Tasks = () => {
             ) : (
                 <h3>No tasks found</h3>
             )}
+            <Modal show={taskEditModal} onHide={toggleModalEditTask}>
+                <Modal.Body>
+                    <Form>
+                        <FormGroup>
+                            <FormControl
+                                value={taskName}
+                                onChange={(e) => setTaskName(e.target.value)}
+                                type="text"
+                                placeholder="Enter new board name"
+                                required
+                            />
+                            <FormControl
+                                value={taskDescription}
+                                onChange={(e) => setTaskDescription(e.target.value)}
+                                type="text"
+                                placeholder="Enter new board description"
+                                required
+                            />
+                        </FormGroup>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={toggleModalEditTask}>Cancel</Button>
+                    <Button variant="secondary" onClick={handelEditTask}>Edit Task!</Button>
+                </Modal.Footer>
+            </Modal>
+            <Modal show={statusChangeModal} onHide={toggleModalChange}>
+                <Modal.Body>
+                    <Form>
+                        <FormGroup>
+                            <FormCheck
+                                type="radio"
+                                label="To Do"
+                                value="to_do"
+                                checked={status === 'to_do'}
+                                onChange={handleStatusChange}
+                            />
+                            <FormCheck
+                                type="radio"
+                                label="In Progress"
+                                value="in_Progress"
+                                checked={status === 'in_Progress'}
+                                onChange={handleStatusChange}
+                            />
+                            <FormCheck
+                                type="radio"
+                                label="Completed"
+                                value="completed"
+                                checked={status === 'completed'}
+                                onChange={handleStatusChange}
+                            />
+                        </FormGroup>
+                    </Form>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={submitCheckBox}>Change status task!</Button>
+                    </Modal.Footer>
+                </Modal.Body>
+            </Modal>
             <Modal show={showModalAddTask} onHide={toggleModalAddTask}>
                 <Modal.Header>
                     <ModalTitle>Add task</ModalTitle>
@@ -91,6 +224,12 @@ const Tasks = () => {
                                 onChange={(e) => setTaskName(e.target.value)}
                                 type="text"
                                 placeholder="Enter board name"
+                                required
+                            />
+                            <FormControl
+                                onChange={(e) => setTaskDescription(e.target.value)}
+                                type="text"
+                                placeholder="Enter board description"
                                 required
                             />
                         </FormGroup>
